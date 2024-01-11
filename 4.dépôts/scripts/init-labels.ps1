e8d98566be9107ce206e47d15505716b664eed18
ESSARRAJ Fouad <fouad.systems@gmail.com>
2024-01-11 12:33:22 +0100

    init-labels

---
+++ 4.dépôts/scripts/init-labels.ps1
@@ -0,0 +1,9 @@
+gh label delete documentation --yes
+gh label delete duplicate --yes
+gh label delete 'good first issue' --yes
+gh label delete 'help wanted' --yes
+gh label delete invalid --yes
+gh label delete question --yes
+gh label delete wontfix --yes
+
+gh label clone https://github.com/labs-web/lab-markdown.git
